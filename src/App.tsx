06128f2e1658c792bac069bc7d449c87cdaceafb
electrixky <tanyatkachuk2401@gmail.com>
2024-02-10 12:20:50 +0200

    Rating refactored

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {OnOff} from "./OnOff";
 import {Accordion} from "./Accordion";
 import {Rating} from "./Rating";
+import {RatingControlled, RatingValueType} from "./RatingControlled";
 
 function App() {
+
+    let [ratingValue, setRatingValue] = useState<RatingValueType>(0)
+
     return (
         <div className="App">
             {/*<OnOff/>*/}
             {/*<OnOff/>*/}
             {/*<OnOff/>*/}
-            <Accordion titleValue={"My Accordion"}/>
+            {/*<Accordion titleValue={"My Accordion"}/>*/}
             {/*<Rating/>*/}
+            <RatingControlled value={ratingValue} onClick={setRatingValue}/>
         </div>
     );
 }
